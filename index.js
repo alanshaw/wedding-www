25b88227101031e82ee2830b429ba0de0de0d642
Alan Shaw <alan@tableflip.io>
2015-02-07 13:59:32 +0000

    Add payload validation

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 var Hapi = require('hapi')
+var Joi = require('joi')
 
 var server = new Hapi.Server()
 server.connection({port: process.env.PORT || 6060})
 
-/*server.route({
+server.route({
   method: 'POST',
-  path: '/register',
-  handler: handlers.register.create,
+  path: '/rsvp',
+  handler: function (req, reply) {
+    console.log(req.payload)
+    reply.redirect('/thanks.html')
+  },
   config: {
-    // TODO: Add payload validation
+    validate: {
+      payload: {
+        name: Joi.string().trim().required(),
+        email: Joi.string().email().required(),
+        dietary: Joi.string().trim().allow(''),
+        transport: Joi.boolean().required()
+      }
+    }
   }
-})*/
+})
 
 server.route({
   method: 'GET',
